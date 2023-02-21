479a5c919def05141d10b1924abfaf94da60392c
parthgu <gparth02@hotmail.com>
2023-02-21 14:49:28 -0800

    added normal to myGame

--- 452_FinalProject/src/my_game/my_game.js
+++ 452_FinalProject/src/my_game/my_game.js
@@ -1,66 +1,67 @@
-"use strict";  // Operate in Strict mode such that variables must be declared before used!
+"use strict"; // Operate in Strict mode such that variables must be declared before used!
 
 import engine from "../engine/index.js";
 
 class MyGame extends engine.Scene {
-    constructor() {
-        super();
-        this.kBg = "assets/bg.png";
+  constructor() {
+    super();
+    this.kBg = "assets/bg.png";
+    this.kBgNormal = "assets/bg_normal.png";
 
-        // The camera to view the scene
-        this.mCamera = null;
-    }
+    // The camera to view the scene
+    this.mCamera = null;
+  }
 
-    load() {
-        engine.texture.load(this.kBg);
-    }
+  load() {
+    engine.texture.load(this.kBg);
+    engine.texture.load(this.kBgNormal);
+  }
 
-    unload() {
-        engine.texture.unload(this.kBg);
-    }
+  unload() {
+    engine.texture.unload(this.kBg);
+    engine.texture.unload(this.kBgNormal);
+  }
 
-    init() {
-        // Step A: set up the cameras
-        this.mCamera = new engine.Camera(
-            vec2.fromValues(50, 36), // position of the camera
-            100,                       // width of camera
-            [0, 0, 640, 330]           // viewport (orgX, orgY, width, height)
-        );
-        this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-        // sets the background to gray
+  init() {
+    // Step A: set up the cameras
+    this.mCamera = new engine.Camera(
+      vec2.fromValues(50, 36), // position of the camera
+      100, // width of camera
+      [0, 0, 640, 330] // viewport (orgX, orgY, width, height)
+    );
+    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+    // sets the background to gray
 
-        // Large background image
-        let bgR = new engine.SpriteRenderable(this.kBg);
-        bgR.setElementPixelPositions(0, 1024, 0, 1024);
-        bgR.getXform().setSize(150, 150);
-        bgR.getXform().setPosition(50, 35);
-        this.mBg = new engine.GameObject(bgR);
-    }
+    // Large background image
+    let bgR = new engine.SpriteRenderable(this.kBg);
+    bgR.setElementPixelPositions(0, 1024, 0, 1024);
+    bgR.getXform().setSize(150, 150);
+    bgR.getXform().setPosition(50, 35);
+    this.mBg = new engine.GameObject(bgR);
+  }
 
-    _drawCamera(camera) {
-        camera.setViewAndCameraMatrix();
-        this.mBg.draw(camera);
-    }
+  _drawCamera(camera) {
+    camera.setViewAndCameraMatrix();
+    this.mBg.draw(camera);
+  }
 
-    // This is the draw function, make sure to setup proper drawing environment, and more
-    // importantly, make sure to _NOT_ change any state.
-    draw() {
-        // Step A: clear the canvas
-        engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+  // This is the draw function, make sure to setup proper drawing environment, and more
+  // importantly, make sure to _NOT_ change any state.
+  draw() {
+    // Step A: clear the canvas
+    engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
-        // Step  B: Draw with all three cameras
-        this._drawCamera(this.mCamera);
-    }
-    // The update function, updates the application state. Make sure to _NOT_ draw
-    // anything from this function!
-    update() {
-
-    }
+    // Step  B: Draw with all three cameras
+    this._drawCamera(this.mCamera);
+  }
+  // The update function, updates the application state. Make sure to _NOT_ draw
+  // anything from this function!
+  update() {}
 }
 
 window.onload = function () {
-    engine.init("GLCanvas");
+  engine.init("GLCanvas");
 
-    let myGame = new MyGame();
-    myGame.start();
-}
+  let myGame = new MyGame();
+  myGame.start();
+};
