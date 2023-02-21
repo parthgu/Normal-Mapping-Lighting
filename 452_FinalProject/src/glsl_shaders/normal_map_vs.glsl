45a8611494e8a330dcfc72c35c0d62884550e636
cpand2 <cpand@uw.edu>
2023-02-21 12:54:15 -0800

    revised shaders for normal map support

--- 452_FinalProject/src/glsl_shaders/normal_map_vs.glsl
+++ 452_FinalProject/src/glsl_shaders/normal_map_vs.glsl
@@ -1,8 +1,10 @@
 attribute vec3 aVertexPosition;      // Vertex shader expects one vertex position
 attribute vec2 aTextureCoordinate;   // This is the texture coordinate attribute
+attribute vec2 aNormalCoordinate;   // This is the texture coordinate attribute
 
 // texture coordinate that maps image to the square
 varying vec2 vTexCoord;
+varying vec2 vNormalCoord;
 varying vec3 fragPos;
 
 // to transform the vertex position
@@ -10,12 +12,9 @@ uniform mat4 uModelXformMatrix;
 uniform mat4 uCameraXformMatrix;
 
 void main(void) { 
-    // Convert the vec3 into vec4 for scan conversion and
-    // transform by uModelXformMatrix and uCameraXformMatrix before
-    // assign to gl_Position to pass the vertex to the fragment shader
     gl_Position = uCameraXformMatrix * uModelXformMatrix * vec4(aVertexPosition, 1.0);
     
-    // pass the texture coordinate to the fragment shader
     vTexCoord = aTextureCoordinate;
+    vNormalCoord = aNormalCoordinate;
     fragPos = vec3(uModelXformMatrix * vec4(aVertexPosition, 1.0));
 }
