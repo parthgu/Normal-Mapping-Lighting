6f6941757802be2beaaf7608f22a52921a1ad5ca
parthgu <gparth02@hotmail.com>
2023-02-21 13:54:32 -0800

    shader changes

--- 452_FinalProject/src/engine/shaders/normal_map_shader.js
+++ 452_FinalProject/src/engine/shaders/normal_map_shader.js
@@ -9,10 +9,38 @@ class NormalMapShader extends TextureShader {
     super(vertexShaderPath, fragmentShaderPath); // call SimpleShader constructor
 
     let gl = glSys.get();
-    this.mNormalMapTextureRef = gl.getUniformLocation(
+
+    this.mTextureRef = gl.getUniformLocation(
       this.mCompiledShader,
-      "uNormalMapTexture"
+      "textureSampler"
     );
+
+    this.mNormalRef = gl.getUniformLocation(
+      this.mCompiledShader,
+      "normalSampler"
+    );
+
+    this.mCameraPosRef = gl.getUniformLocation(
+      this.mCompiledShader,
+      "uCameraPos"
+    );
+
+    this.mLightPosRef = gl.getUniformLocation(
+      this.mCompiledShader,
+      "uLightPos"
+    );
+  }
+
+  activate(pixelColor, trsMatrix, cameraMatrix) {
+    // first call the super class' activate
+    super.activate(pixelColor, trsMatrix, cameraMatrix);
+
+    // now our own functionality: enable texture coordinate array
+    let gl = glSys.get();
+
+    // bind uSampler to texture 0
+    gl.uniform1i(this.mTextureRef, 0); // texture.activateTexture() binds to Texture0
+    gl.uniform1i(this.mNormalRef, 1);
   }
 }
 
