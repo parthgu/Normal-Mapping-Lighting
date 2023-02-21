7722bc9611383bda669bf77c05a471aeabca07bf
parthgu <gparth02@hotmail.com>
2023-02-21 14:25:55 -0800

    changes to chader

--- 452_FinalProject/src/engine/shaders/normal_map_shader.js
+++ 452_FinalProject/src/engine/shaders/normal_map_shader.js
@@ -8,6 +8,8 @@ class NormalMapShader extends TextureShader {
     // Call super class constructor
     super(vertexShaderPath, fragmentShaderPath); // call SimpleShader constructor
 
+    this.mLightPos = [0, 0, 1];
+
     let gl = glSys.get();
 
     this.mTextureRef = gl.getUniformLocation(
@@ -31,9 +33,9 @@ class NormalMapShader extends TextureShader {
     );
   }
 
-  activate(pixelColor, trsMatrix, cameraMatrix) {
+  activate(pixelColor, trsMatrix, camera) {
     // first call the super class' activate
-    super.activate(pixelColor, trsMatrix, cameraMatrix);
+    super.activate(pixelColor, trsMatrix, camera.getCameraMatrix());
 
     // now our own functionality: enable texture coordinate array
     let gl = glSys.get();
@@ -41,6 +43,13 @@ class NormalMapShader extends TextureShader {
     // bind uSampler to texture 0
     gl.uniform1i(this.mTextureRef, 0); // texture.activateTexture() binds to Texture0
     gl.uniform1i(this.mNormalRef, 1);
+    gl.uniform3f(this.mCameraPosRef, camera.getCameraPosVector());
+    gl.uniform3f(this.mLightPosRef, this.mLightPos);
+  }
+
+  setLightPos(xForm) {
+    this.mLightPos[0] = xForm.getXPos();
+    this.mLightPos[1] = xForm.getYPos();
   }
 }
 
