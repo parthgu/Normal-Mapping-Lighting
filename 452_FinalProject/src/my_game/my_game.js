505e8bfcf2d645a2a4f6e48f4631ef1f770d9348
parthgu <gparth02@hotmail.com>
2023-02-21 15:25:41 -0800

    created test objects

--- 452_FinalProject/src/my_game/my_game.js
+++ 452_FinalProject/src/my_game/my_game.js
@@ -25,25 +25,31 @@ class MyGame extends engine.Scene {
   init() {
     // Step A: set up the cameras
     this.mCamera = new engine.Camera(
-      vec2.fromValues(50, 36), // position of the camera
+      vec2.fromValues(50, 40), // position of the camera
       100, // width of camera
       [0, 0, 640, 330] // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     // sets the background to gray
 
+    this.lightSource = new engine.LightSource();
+    this.lightSource.getXform().setXPos(50);
+    this.lightSource.getXform().setYPos(40);
+
     // Large background image
-    let bgR = new engine.SpriteRenderable(this.kBg);
-    bgR.setElementPixelPositions(0, 1024, 0, 1024);
-    bgR.getXform().setSize(150, 150);
-    bgR.getXform().setPosition(50, 35);
-    this.mBg = new engine.GameObject(bgR);
+    this.bgR = new engine.NormalMapRenderable(
+      this.kBg,
+      this.kBgNormal,
+      this.lightSource
+    );
+    this.bgR.getXform().setSize(150, 150);
+    this.bgR.getXform().setPosition(50, 40);
   }
 
-  _drawCamera(camera) {
-    camera.setViewAndCameraMatrix();
-    this.mBg.draw(camera);
-  }
+  //   _drawCamera(camera) {
+  //     camera.setViewAndCameraMatrix();
+  //     this.mBg.draw(camera);
+  //   }
 
   // This is the draw function, make sure to setup proper drawing environment, and more
   // importantly, make sure to _NOT_ change any state.
@@ -52,7 +58,7 @@ class MyGame extends engine.Scene {
     engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
     // Step  B: Draw with all three cameras
-    this._drawCamera(this.mCamera);
+    this.bgR.draw(this.mCamera);
   }
   // The update function, updates the application state. Make sure to _NOT_ draw
   // anything from this function!
