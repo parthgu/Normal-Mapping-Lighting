a462d4a976255335aa9674c4945f303f5b11212b
parthgu <gparth02@hotmail.com>
2023-02-21 15:25:28 -0800

    fixed variable names

--- 452_FinalProject/src/glsl_shaders/normal_map_fs.glsl
+++ 452_FinalProject/src/glsl_shaders/normal_map_fs.glsl
@@ -15,14 +15,14 @@ uniform vec3 uLightPos;
 
 // The "varying" keyword is for signifying that the texture coordinate will be
 // interpolated and thus varies. 
-varying vec2 vTextureCoord;
+varying vec2 vTexCoord;
 varying vec3 vFragPos;
 
 vec3 lightColor = vec3(1.0, 1.0, 1.0);
 
 void main(void)  {
-    vec4 textureColor = texture2D(textureSampler, vec2(vTextureCoord.s, vTextureCoord.t));
-    vec3 normal = vec3(texture2D(normalSampler, vec2(vTextureCoord.s, vTextureCoord.t)));
+    vec4 textureColor = texture2D(textureSampler, vec2(vTexCoord.s, vTexCoord.t));
+    vec3 normal = vec3(texture2D(normalSampler, vec2(vTexCoord.s, vTexCoord.t)));
     normal = normalize(normal);
     
     vec4 result = textureColor;
@@ -32,8 +32,8 @@ void main(void)  {
 
         vec3 ambient = 0.1 * lightColor;
         vec3 diffuse = max(0.0, dot(normal, normalize(uLightPos - vFragPos))) * lightColor;
-        vec3 specular = pow(max(0.0, dot(normalize(uCameraPos - vFragPos), lightReflect)), 16) * 0.5 * lightColor;
-
+        // vec3 specular = pow(max(0.0, dot(normalize(uCameraPos - vFragPos), lightRefle
+        vec3 specular = max(0.0, dot(normalize(uCameraPos - vFragPos), lightReflect)) * 0.5 * lightColor;
         result = vec4((ambient + diffuse + specular) * vec3(result), 1.0);
     }
     
--- 452_FinalProject/src/glsl_shaders/normal_map_vs.glsl
+++ 452_FinalProject/src/glsl_shaders/normal_map_vs.glsl
@@ -4,7 +4,7 @@ attribute vec2 aTextureCoordinate;   // This is the texture coordinate attribute
 // texture coordinate that maps image to the square
 varying vec2 vTexCoord;
 varying vec2 vNormalCoord;
-varying vec3 fragPos;
+varying vec3 vFragPos;
 
 // to transform the vertex position
 uniform mat4 uModelXformMatrix;
@@ -14,5 +14,5 @@ void main(void) {
     gl_Position = uCameraXformMatrix * uModelXformMatrix * vec4(aVertexPosition, 1.0);
     
     vTexCoord = aTextureCoordinate;
-    fragPos = vec3(uModelXformMatrix * vec4(aVertexPosition, 1.0));
+    vFragPos = vec3(uModelXformMatrix * vec4(aVertexPosition, 1.0));
 }
