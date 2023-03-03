31d517c048149c004d8e9df44a0abd0a9940e5dc
parthgu <gparth02@hotmail.com>
2023-03-03 01:40:21 -0700

    made flicker slower

--- src/my_game/stone_scene.js
+++ src/my_game/stone_scene.js
@@ -54,7 +54,7 @@ class MyGame extends engine.Scene {
     this.mMsg2.setColor([1, 1, 1, 1]);
     this.mMsg2.getXform().setPosition(5, 5);
     this.mMsg2.setTextHeight(3);
-    this.lightFlicker = new engine.Oscillate(0.2, 40, 350);
+    this.lightFlicker = new engine.Oscillate(0.2, 40, 450);
   }
 
   //   _drawCamera(camera) {
