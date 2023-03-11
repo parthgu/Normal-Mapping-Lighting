40eb53d40425a0eb1e8677d6771defeb1be9d03b
cpand2 <cpand@uw.edu>
2023-03-10 16:39:07 -0800

    added model space position logic in frag shader

--- src/glsl_shaders/normal_map_fs.glsl
+++ src/glsl_shaders/normal_map_fs.glsl
@@ -4,12 +4,18 @@ uniform sampler2D textureSampler;
 uniform sampler2D normalSampler;
 uniform vec4 uAmbientColor;
 
+// light 1 uniforms
 uniform vec3 uLightPos;
 uniform vec4 uLightColor;
 uniform vec3 uFalloff;
 uniform bool uHasDiffuse;
 uniform bool uHasSpec;
 
+uniform vec3 uLightPosModelSpace;
+uniform bool uIsInWC;
+uniform mat4 uModelRTMat;
+
+// light 2 uniforms
 uniform bool isSecondLightActive;
 uniform vec3 uLightPos2;
 uniform vec4 uLightColor2;
@@ -20,11 +26,10 @@ uniform bool uHasSpec2;
 varying vec2 vTexCoord;
 varying vec3 vFragPos;
 
+// constants
 vec3 normalZero = vec3(0.5, 0.5, 0.5);
-
 float diffuseWeight = 0.5;
 float specularWeight = 0.5;
-
 float minWeightThresh = 0.6;
 
 void main(void)  {
@@ -32,20 +37,25 @@ void main(void)  {
     vec3 normal = texture2D(normalSampler, vTexCoord).rgb;
     normal = normalize(normal - normalZero);
     
+    vec3 lightPosToUse = uLightPos;
+    if (!uIsInWC) {
+        lightPosToUse = vec3(uModelRTMat * vec4(uLightPosModelSpace, 1.0));
+    }
+
     vec4 result = textureColor;
     vec3 Ambient = uAmbientColor.rgb * uAmbientColor.a;
 
     if (textureColor.a > 0.9) {
-        vec3 lightIncident = normalize(vFragPos - uLightPos);
+        vec3 lightIncident = normalize(vFragPos - lightPosToUse);
         vec3 lightReflect = reflect(lightIncident, normal);
 
-        float D = length(uLightPos - vFragPos);
+        float D = length(lightPosToUse - vFragPos);
         float Attenuation = 1.0 /
             (uFalloff.x + (uFalloff.y*D) + (uFalloff.z*D*D));
         
         vec3 diffuse = vec3(0.0, 0.0, 0.0);
         if (uHasDiffuse) {
-            float diffuseTerm = max(0.0, dot(normal, normalize(uLightPos - vFragPos)))
+            float diffuseTerm = max(0.0, dot(normal, normalize(lightPosToUse - vFragPos)))
                 * Attenuation;
             if (diffuseTerm > minWeightThresh) diffuseTerm = minWeightThresh;
             else diffuseTerm = 0.0;
@@ -63,6 +73,7 @@ void main(void)  {
             specular = specTerm * uLightColor.rgb * uLightColor.a;
         }
 
+        // second light logic
         if (isSecondLightActive) {
             lightIncident = normalize(vFragPos - uLightPos2);
             lightReflect = reflect(lightIncident, normal);
